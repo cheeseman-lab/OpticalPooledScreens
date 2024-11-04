8f64921fdde7155b2ee1d2e0778c7faf41671d10
mdiberna <mdiberna@fry.wi.mit.edu>
2024-11-04 09:30:26 -0500

    change package imports for agg. in the example_analysis

--- example_analysis/aggregate_4/tile-based/aggregate_4_eval.py
+++ example_analysis/aggregate_4/tile-based/aggregate_4_eval.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import ops.aggregate as agg 
+import ops.aggregate as aggregate 
 
 # Set screen directories
 ph_function_home = "/lab/barcheese01/screens"
@@ -21,10 +21,10 @@ filter_single_gene = False
 channels = ['dapi', 'cenpa', 'coxiv', 'wga']
 
 print("Loading subsets of processed datasets...")
-raw_df = agg.load_hdf_subset('merge_3/hdf/merged_final.hdf', n_rows=50000)
-clean_df = agg.clean_cell_data(raw_df, population_feature, filter_single_gene=filter_single_gene)
-transformed_df = agg.load_hdf_subset('aggregate_4/hdf/transformed_data.hdf', n_rows=clean_df.shape[0])
-standardized_df = agg.load_hdf_subset('aggregate_4/hdf/standardized_data.hdf', n_rows=clean_df.shape[0])
+raw_df = aggregate.load_hdf_subset('merge_3/hdf/merged_final.hdf', n_rows=50000)
+clean_df = aggregate.clean_cell_data(raw_df, population_feature, filter_single_gene=filter_single_gene)
+transformed_df = aggregate.load_hdf_subset('aggregate_4/hdf/transformed_data.hdf', n_rows=clean_df.shape[0])
+standardized_df = aggregate.load_hdf_subset('aggregate_4/hdf/standardized_data.hdf', n_rows=clean_df.shape[0])
 dfs = {
     "clean": clean_df,
     "transformed": transformed_df, 
@@ -36,11 +36,11 @@ cell_features = ['cell_{}_mean'.format(channel) for channel in channels]
 nucleus_features = ['nucleus_{}_mean'.format(channel) for channel in channels]
 
 plt.figure(figsize=(12, 6))
-agg.plot_feature_distributions(dfs, cell_features, remove_clean=True)
+aggregate.plot_feature_distributions(dfs, cell_features, remove_clean=True)
 save_plot(plt.gcf(), 'cell_feature_violins.png')
 
 plt.figure(figsize=(12, 6))
-agg.plot_feature_distributions(dfs, nucleus_features, remove_clean=True)
+aggregate.plot_feature_distributions(dfs, nucleus_features, remove_clean=True)
 save_plot(plt.gcf(), 'nuclear_feature_violins.png')
 
 print("Testing for missing values in final dataframes...")
@@ -48,9 +48,9 @@ mitotic = pd.read_csv('aggregate_4/csv/mitotic_gene_data.csv')
 interphase = pd.read_csv('aggregate_4/csv/interphase_gene_data.csv')
 all = pd.read_csv('aggregate_4/csv/all_gene_data.csv')
 
-mitotic_missing = agg.test_missing_values(mitotic, 'mitotic')
-interphase_missing = agg.test_missing_values(interphase, 'interphase')
-all_missing = agg.test_missing_values(all, 'all')
+mitotic_missing = aggregate.test_missing_values(mitotic, 'mitotic')
+interphase_missing = aggregate.test_missing_values(interphase, 'interphase')
+all_missing = aggregate.test_missing_values(all, 'all')
 
 mitotic_missing.to_csv(os.path.join(qc_dir, 'mitotic_missing.csv'), index=False)
 interphase_missing.to_csv(os.path.join(qc_dir, 'interphase_missing.csv'), index=False)
